f9938b3439c3a45aac7d42f3080637e2946faacb
amnah <amnah@vt.edu>
2019-02-15 16:18:26 -0500

    Wrote script to split directories

--- run_calibration.py
+++ run_calibration.py
@@ -3,14 +3,12 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 # import pandas as pd
-# import matplotlib.pyplot as plt
 
 abfpath = sys.argv[1]
 vg = voltammetry.Data(abfpath)
 labels = voltammetry.Mulabels(abfpath, 'run.csv')
 data = voltammetry.PreprocessedData(vg.Voltammogram, labels)
-# bestAlpha = voltammetry.best_alpha(data.training)
-bestAlpha = 1
+bestAlpha = voltammetry.best_alpha(data.training)
 t = np.linspace(0, 1032 * len(data.testing.labels) / vg.samplingRate, len(data.testing.labels))
 cvFit = voltammetry.train_analyte(data.training, alpha=bestAlpha)
 predictions = voltammetry.test_analyte(data.testing, cvFit)
