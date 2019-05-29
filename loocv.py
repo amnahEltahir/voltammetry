edb15721630e96e8c70a8ba3a3dcdbcd1f67ec34
amnah <amnah@vt.edu>
2019-05-29 14:23:46 -0400

    updated methods to do leave one out cross validation based on concentration, by both manually removing a concentration for training and ordering the folds.

--- loocv.py
+++ loocv.py
@@ -1,7 +1,7 @@
 import voltammetry
 import sys
 import matplotlib.pyplot as plt
-
+import numpy as np
 
 
 abfpath = sys.argv[1]
@@ -15,7 +15,8 @@ t = vg.sweep_point_count * data.testing.index / vg.samplingRate
 cvFit = voltammetry.train_analyte(data.training, alpha=bestAlpha)
 predictions = voltammetry.test_analyte(data.testing, cvFit)
 for chemIx in range(len(labels.targetAnalyte)):
+    loc_idx = int(np.where(labels.labels==loc)[chemIx])
     stats = voltammetry.calcStepStats(chemIx, predictions, data.testing.labels)
     calFig = voltammetry.plot_Calibration(t, predictions, data.testing.labels, labels.targetAnalyte, chemIx, stats)
-    calFig.suptitle(vg.name)
+    calFig.suptitle(''.join(("Train Excluding ", str(loc), " nM")))
     plt.show()
